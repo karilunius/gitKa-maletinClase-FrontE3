1903dfbce33668593994f392017049c226adc2cf
karilunius <93058053+karilunius@users.noreply.github.com>
2023-03-05 16:26:30 -0500

    practico counter

---
+++ clase9-m7-counter2versions/counter/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { AppFrame } from './styled/StyledComp';
+
+function App() {
+
+
+  return (
+    <AppFrame>
+      <p>hola</p>
+    </AppFrame>
+  )
+}
+
+export default App
---
+++ clase9-m7-counter2versions/counter/src/styled/StyledComp.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export const AppFrame = styled.div `
+	background-color: #242424;
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: center;
+	padding-top: 15px;
+	font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+`;
+
